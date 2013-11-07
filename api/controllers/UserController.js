b5e882a6686d0bbafa2b6670123c3b0da14f8efb
jpopesculian <jpopesculian@gmail.com>
2013-11-07 12:10:07 -0500

    changed signup.ejs

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -17,7 +17,18 @@
 
 module.exports = {
     
-  
+  create: function(req, res) {
+  	User.create({
+  		username: req.param('username'),
+  		email: req.param('email')
+  	}).done(function(err, user) {
+		if (err) {
+			return console.log(err);
+		} else {
+		console.log("User created:", user.username);
+		}
+	});
+  },
 
 
   /**
--- api/models/User.js
+++ api/models/User.js
@@ -14,7 +14,7 @@ module.exports = {
   		type: 'string',
   		required: true
   	},
-  	emailAddress: {
+  	email: {
       type: 'email',
       required: true
     }
