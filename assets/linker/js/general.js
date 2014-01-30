8c268fd6ed3159328bcd9d5f21af409a2a00e546
Julian Popescu <jpopesculian@gmail.com>
2014-01-29 22:21:07 -0500

    added redirection function

--- assets/linker/js/general.js
+++ assets/linker/js/general.js
@@ -1,3 +1,11 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+function goTo(url, leave_history) {
+	if (typeof leave_history !== 'undefined') {
+		window.location.href(url);
+	} else {
+		window.location.replace(url);
+	}
+}
