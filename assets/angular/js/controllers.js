0741be8b99bcab4a70d911e7791f70f77822b9cd
Kelsay Lewis <kelsaylewis@gmail.com>
2014-03-04 17:12:21 -0500

    calling the function get_events_by_creator

--- assets/angular/js/controllers.js
+++ assets/angular/js/controllers.js
@@ -89,13 +89,13 @@ streamSyncControllers.controller('ProfileCtrl', ['$scope', '$http',
         $scope.logged_in();
 
         $scope.current_user_created_events = [];
-        Event.get_events_by_creator = function(){
+        $scope.get_events_by_creator = function(){
             $http.get('event/get_events_by_creator')
                 .success(function(data, status){
                     console.log(data);
                     $scope.current_user_created_events = data.events;
                 });
             };
-            $scope.get_events_by_creator();
+        $scope.get_events_by_creator();
 
     }]); 
