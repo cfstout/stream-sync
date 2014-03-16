fe84ec12398a7842e35541d73292a523a29b0f0b
Julian Popescu <jpopesculian@gmail.com>
2014-03-16 18:53:15 -0400

    trying cors stuff again

--- config/passport.js
+++ config/passport.js
@@ -3,11 +3,26 @@
 var passport    = require('passport'),
   LocalStrategy = require('passport-local').Strategy;
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 module.exports = {
  express: {
     customMiddleware: function(app){
-      app.use(passport.initialize());
-      app.use(passport.session());
+        app.use(allowCrossDomain);
+        app.use(passport.initialize());
+        app.use(passport.session());
     }
   }
 };
