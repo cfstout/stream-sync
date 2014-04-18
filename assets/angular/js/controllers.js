b4598d193f0c6a2118aaf32f55ee401faeda3a5e
Julian Popescu <jpopesculian@gmail.com>
2014-04-17 20:13:14 -0400

    formatting OCD

--- assets/angular/js/controllers.js
+++ assets/angular/js/controllers.js
@@ -4,123 +4,123 @@
 
 var streamSyncControllers = angular.module('streamSyncControllers', []);
 
-streamSyncControllers.controller('HomeCtrl', ['$scope','user',
-  function($scope,user){
-    $scope.existing = function(){
-      user.existing(this.user);
-    };
-  }]);
+streamSyncControllers.controller('HomeCtrl', ['$scope', 'user',
+    function ($scope, user) {
+        $scope.existing = function () {
+            user.existing(this.user);
+        };
+    }]);
 /**
 *   Login form [templates/user/login]
 */
 streamSyncControllers.controller('LoginCtrl', ['$scope', 'user',
-  function($scope, user) {
-    // Parameters to Request
-    $scope.username = "";
-    $scope.password = "";
+    function ($scope, user) {
+        // Parameters to Request
+        $scope.username = "";
+        $scope.password = "";
 
-    //Request to server to perform action
-    $scope.submit = function() {
-        user.login(this.username, this.password);
-    };
+        //Request to server to perform action
+        $scope.submit = function () {
+            user.login(this.username, this.password);
+        };
 
-  }]);
+    }]);
 
 /**
 *   Signup form [templates/user/signup]
 */
 streamSyncControllers.controller('SignupCtrl', ['$scope', 'user',
-  function($scope, user) {
-    // Parameters to Request
-    $scope.username = "";
-    $scope.password = "";
-    $scope.loc = "";
-    $scope.email = "";
+    function ($scope, user) {
+        // Parameters to Request
+        $scope.username = "";
+        $scope.password = "";
+        $scope.loc = "";
+        $scope.email = "";
 
-    //Request to server to perform action
-    $scope.submit = function() {
-    	user.signup(this.username, this.password, this.loc, this.email);
-    };
+        //Request to server to perform action
+        $scope.submit = function () {
+            user.signup(this.username, this.password, this.loc, this.email);
+        };
 
-  }]);
+    }]);
 
 streamSyncControllers.controller('NavCtrl', ['$scope', '$location', '$rootScope', 'user',
-  function($scope, $location, $rootScope, user) {
-    $scope.logout = function() {
-      user.logout();
-    };
-    $scope.base_path = $location.path().split('/')[1];
+    function ($scope, $location, $rootScope, user) {
+        $scope.logout = function () {
+            user.logout();
+        };
+        $scope.base_path = $location.path().split('/')[1];
 
-    $rootScope.hints = false;
-    $scope.toggleHints = function(){
-      $rootScope.hints = !$rootScope.hints;
-    };
-  }]);
+        $rootScope.hints = false;
+        $scope.toggleHints = function () {
+            $rootScope.hints = !$rootScope.hints;
+        };
+    }]);
 
-streamSyncControllers.controller('EventSongCtrl', ['$scope','$http',
-  function($scope,$http){
-    console.log($scope.playlistID);
-    $scope.getSongs = function(playlistID){
-      $http.get('/playlist/' + playlistID + '/getSongs')
-        .error(function (data, status) {
-          console.log("ERROR");
-        })
-        .success(function (data,status){
-          $scope.songs = data.songs;
-        });
-    };
-    $scope.getSongs($scope.playlistID);
+streamSyncControllers.controller('EventSongCtrl', ['$scope', '$http',
+    function ($scope, $http) {
+        console.log($scope.playlistID);
+        $scope.getSongs = function (playlistID) {
+            $http.get('/playlist/' + playlistID + '/getSongs')
+                .error(function (data, status) {
+                    console.log("ERROR");
+                })
+                .success(function (data, status) {
+                    $scope.songs = data.songs;
+                });
+        };
+        $scope.getSongs($scope.playlistID);
 
-  }]);
+    }]);
 
 streamSyncControllers.controller('EventCreateCtrl', ['$scope', 'event', 'user',
-    function($scope, event, user) {
+    function ($scope, event, user) {
         //paramaters to request
         $scope.name = "";
         $scope.date = "";
         user.logged_in();
 
         //request server to perform action
-        $scope.submit = function() {
-           event.create(this.name, this.date);
+        $scope.submit = function () {
+            event.create(this.name, this.date);
         };
-    }]); 
+    }]);
 
-streamSyncControllers.controller('EventDeleteCtrl',['$scope', '$http','$location',
-  function($scope,$http,$location){
-    $scope.delete = function(){
-      $http.delete('/event/'+$scope.eventID + '/delete')
-        .error(function (data, status) {
-          console.log("ERROR");
-        })
-        .success(function (data, status){
-          console.log("SUCCESS");
-          location.reload();
-        });
-      };
-  }])
+streamSyncControllers.controller('EventDeleteCtrl', ['$scope', '$http', '$location',
+    function ($scope, $http, $location) {
+        $scope.delete = function () {
+            $http.delete('/event/' + $scope.eventID + '/delete')
+                .error(function (data, status) {
+                    console.log("ERROR");
+                })
+                .success(function (data, status){
+                    console.log("SUCCESS");
+                    $location.reload();
+                });
+        };
+    }]);
 
 streamSyncControllers.controller('ProfileCtrl', ['$scope', 'user', 'event',
-    function($scope, user, event) {
+    function ($scope, user, event) {
         $scope.current_user = {};
         user.logged_in()
-            .success(function(data, status) {
+            .success(function (data, status) {
                 $scope.current_user = data.user;
             });
 
         $scope.current_user_created_events = [];
         // returns the events created by the current user
-        $scope.get_events_by_creator = function(){
+        $scope.get_events_by_creator = function () {
             event.get_events_by_creator()
-                .success(function(data, status){
+                .success(function (data, status) {
                     $scope.current_user_created_events = data.events;
                 });
-            };
+        };
         $scope.get_events_by_creator();
-    }]); 
+    }]);
 
 streamSyncControllers.controller('EventListCtrl', ['$scope', 'user', 'event',
-    function($scope, user, event) {
+    function ($scope, user, event) {
 
         //List of events to display
         $scope.events = [];
@@ -129,129 +129,128 @@ streamSyncControllers.controller('EventListCtrl', ['$scope', 'user', 'event',
         $scope.user = {};
 
         user.logged_in()
-          .success(function(data, status) {
-              $scope.user = data.user;
-          });
+            .success(function (data, status) {
+                $scope.user = data.user;
+            });
 
         //Function to fetch lists from database
-        $scope.search = function() {
-          event.search(this.query)
-            .success(function(data, status) {
-              $scope.events = data.events;
-            });
+        $scope.search = function () {
+            event.search(this.query)
+                .success(function (data, status) {
+                    $scope.events = data.events;
+                });
         };
 
         //Calls the function to populate the event list
         $scope.search('');
 
-    }]); 
+    }]);
 
 streamSyncControllers.controller('PlayBackCtrl', ['$scope', '$routeParams', 'event', 'playlist', 'memberlist', 'user', 'song', 'time',
-  function($scope, $routeParams, event, playlist, memberlist, user, song, time) {
-    // get logged in user
-    $scope.user = {};
+    function ($scope, $routeParams, event, playlist, memberlist, user, song, time) {
+        // get logged in user
+        $scope.user = {};
         user.logged_in()
-            .success(function(data, status) {
+            .success(function (data, status) {
                 $scope.user = data.user;
             });
 
-    $scope.event = {};
-    $scope.memberlist = {};
-    $scope.playlist = {};
-    $scope.isHost = false;
+        $scope.event = {};
+        $scope.memberlist = {};
+        $scope.playlist = {};
+        $scope.isHost = false;
 
-    $scope.updateTime = function() {
-      if ($scope.playlist.curTime) {
-        $scope.playlist.percentPlayed = (100 * $scope.playlist.curTime.real / $scope.playlist.curDuration.real);
-        $scope.playlist.curTime.pretty = time.prettify($scope.playlist.curTime.real);
-      }
-    };
+        $scope.updateTime = function () {
+            if ($scope.playlist.curTime) {
+                $scope.playlist.percentPlayed = (100 * $scope.playlist.curTime.real / $scope.playlist.curDuration.real);
+                $scope.playlist.curTime.pretty = time.prettify($scope.playlist.curTime.real);
+            }
+        };
 
-    $scope.updatePlaylist = function() {
-      $scope.playlist = playlist.instance;
-      $scope.updateTime();
-    };
+        $scope.updatePlaylist = function () {
+            $scope.playlist = playlist.instance;
+            $scope.updateTime();
+        };
 
-    $scope.updateMemberlist = function() {
-      $scope.memberlist = memberlist.instance;
-    };
+        $scope.updateMemberlist = function () {
+            $scope.memberlist = memberlist.instance;
+        };
 
-    // initialize event
-    event.join($routeParams.eventSlug)
-      .success(function (data, status) {
-          $scope.event = data.event;
-          $scope.isHost = data.event.isHost;
-          // set playlist
-          playlist.set.call(playlist, data.event.playlist, $scope.isHost);
-          playlist.watch($scope.updatePlaylist);
-          // set memberlist
-          memberlist.set.call(memberlist, data.event.memberlist, $scope.user);
-          memberlist.watch($scope.updateMemberlist);
-
-      });
-
-    // Search objects
-    $scope.query = "";
-    $scope.results = null;
-    $scope.selectedResult = null;
-
-    // Toggle Search Modes
-    $scope.searchModeOn = function() { $scope.results = []; };
-    $scope.searchModeOff = function() { $scope.results = null; };
-
-    //Request to server to search for a song
-    $scope.search = function() {
-        $scope.results = [];
-        song.search.youtube(this.query)
+        // initialize event
+        event.join($routeParams.eventSlug)
             .success(function (data, status) {
-                $scope.results = $scope.results.concat(song.process.youtube(data.list.items));
+                $scope.event = data.event;
+                $scope.isHost = data.event.isHost;
+                // set playlist
+                playlist.set.call(playlist, data.event.playlist, $scope.isHost);
+                playlist.watch($scope.updatePlaylist);
+                // set memberlist
+                memberlist.set.call(memberlist, data.event.memberlist, $scope.user);
+                memberlist.watch($scope.updateMemberlist);
             });
-        song.search.soundcloud(this.query)
-            .success(function (data, status) {
-                $scope.results = $scope.results.concat(song.process.soundcloud(data.list));
-            });
-    };
 
-    // Select a Result
-    $scope.selectResult = function(index) { $scope.selectedResult = $scope.results[index]; };
-    $scope.unselectResult = function() { $scope.selectedResult = null; };
+        // Search objects
+        $scope.query = "";
+        $scope.results = null;
+        $scope.selectedResult = null;
 
-    // Adding Song
-    $scope.addSelectedSong = function() {
-        song.createRemoteSong(this.selectedResult)
-          .success(function (data, status) {
-              playlist.addSong(data.song);
-              $scope.unselectResult();
-              $scope.searchModeOff();
-          });
-    };
+        // Toggle Search Modes
+        $scope.searchModeOn = function () { $scope.results = []; };
+        $scope.searchModeOff = function () { $scope.results = null; };
 
-    song.initializePlayers();
+        //Request to server to search for a song
+        $scope.search = function () {
+            $scope.results = [];
+            song.search.youtube(this.query)
+                .success(function (data, status) {
+                    $scope.results = $scope.results.concat(song.process.youtube(data.list.items));
+                });
+            song.search.soundcloud(this.query)
+                .success(function (data, status) {
+                    $scope.results = $scope.results.concat(song.process.soundcloud(data.list));
+                });
+        };
 
-    $scope.controls = {
-      play: function() { 
-        playlist.play(); 
-      },
-      pause: function() { 
-        playlist.pause(); 
-      },
-      seek: function(click) {
-        if ($scope.isHost) {
-          var newTime = (click.offsetX / click.target.offsetWidth) * $scope.playlist.curDuration.real;
-          playlist.seek(newTime);
-        }
-      },
-      next: function() {
-        playlist.next_song();
-      },
-      prev: function() {
-        playlist.prev_song();
-      }
-    };
+        // Select a Result
+        $scope.selectResult = function (index) { $scope.selectedResult = $scope.results[index]; };
+        $scope.unselectResult = function () { $scope.selectedResult = null; };
 
-    $scope.$on('$destroy', function() {
-      playlist.stop();
-      memberlist.leave();
-    });
+        // Adding Song
+        $scope.addSelectedSong = function () {
+            song.createRemoteSong(this.selectedResult)
+                .success(function (data, status) {
+                    playlist.addSong(data.song);
+                    $scope.unselectResult();
+                    $scope.searchModeOff();
+                });
+        };
 
-  }]);
+        song.initializePlayers();
+
+        $scope.controls = {
+            play: function () {
+                playlist.play();
+            },
+            pause: function () {
+                playlist.pause();
+            },
+            seek: function (click) {
+                if ($scope.isHost) {
+                    var newTime = (click.offsetX / click.target.offsetWidth) * $scope.playlist.curDuration.real;
+                    playlist.seek(newTime);
+                }
+            },
+            next: function () {
+                playlist.next_song();
+            },
+            prev: function () {
+                playlist.prev_song();
+            }
+        };
+
+        $scope.$on('$destroy', function () {
+            playlist.stop();
+            memberlist.leave();
+        });
+
+    }]);
